b11b787bad38c3c58d285a09478c872f9d1c0ffa
Nestor Eduardo Ribero Vargas <nestor_05ribero@hotmail.com>
2022-04-28 11:29:22 -0500

    Create

---
+++ first-react-jsx/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello Dojo</h1>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt Everest</li>
+          <li>Run a marathon</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
